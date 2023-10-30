e721dd7433cb37576601fc8bfd8fda6525a82ba3
Guilherme Bonato <guilhermeferrazbonato@gmail.com>
2023-10-30 15:00:02 -0300

    AsNoTracking

--- ef6EssencialNetCore/Controllers/ProdutosController.cs
+++ ef6EssencialNetCore/Controllers/ProdutosController.cs
@@ -19,7 +19,7 @@ namespace ef6EssencialNetCore.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<Produto>> Get()
         {
-            var produtos = _context.Produtos.ToList();
+            var produtos = _context.Produtos.AsNoTracking().ToList();
 
             if (produtos is null) 
             {
