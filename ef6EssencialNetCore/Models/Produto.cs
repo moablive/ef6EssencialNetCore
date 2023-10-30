37e740bf6c028e0b1b5ec120bfd30246a2bdb9c6
Guilherme Bonato <guilhermeferrazbonato@gmail.com>
2023-10-30 14:56:15 -0300

    JsonIgnore

--- ef6EssencialNetCore/Models/Produto.cs
+++ ef6EssencialNetCore/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace ef6EssencialNetCore.Models;
 
@@ -30,5 +31,7 @@ public class Produto
 
     //Relacionamento
     public int CategoriaId { get; set; }
+
+    [JsonIgnore]
     public Categoria? Categoria { get; set; }
 }
