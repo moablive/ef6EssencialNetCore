8876a1a45abad83b3bc88d04228098af85584332
Guilherme Bonato <guilhermeferrazbonato@gmail.com>
2023-11-13 11:07:48 -0300

    Raspian HML

--- ef6EssencialNetCore/Migrations/20231113134144_raspianTeste.Designer.cs
+++ ef6EssencialNetCore/Migrations/20231113134144_raspianTeste.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ef6EssencialNetCore.Context;
 
@@ -10,9 +11,11 @@ using ef6EssencialNetCore.Context;
 namespace ef6EssencialNetCore.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231113134144_raspianTeste")]
+    partial class raspianTeste
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
