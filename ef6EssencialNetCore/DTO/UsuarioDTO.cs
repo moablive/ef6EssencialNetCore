0da52b344640a0434ea0731759a0f03c3e6bf6ce
Guilherme Bonato <guilhermeferrazbonato@gmail.com>
2023-12-04 14:38:38 -0300

    formatação

--- ef6EssencialNetCore/DTO/UsuarioDTO.cs
+++ ef6EssencialNetCore/DTO/UsuarioDTO.cs
@@ -3,6 +3,6 @@ namespace ef6EssencialNetCore.DTO;
     public class UsuarioDTO
     {
         public string? Email { get; set; }
-        public string ?Password { get; set; }
+        public string? Password { get; set; }
         public string? ConfirmPassword { get; set; }
     }
